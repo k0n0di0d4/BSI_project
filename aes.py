2b4f6366e8f5dccdd337a27ad35544268f92d3f4
Filip Werra <filipwerra.12@gmail.com>
2021-01-17 13:55:27 +0100

    crypting of messages

--- aes.py
+++ aes.py
@@ -14,8 +14,8 @@ class AESCipher:
     def encrypt(self, data):
         iv = get_random_bytes(AES.block_size)
         self.cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return b64encode(iv + self.cipher.encrypt(pad(data.encode('utf-8'), 
-            AES.block_size)))
+        return b64encode(iv + self.cipher.encrypt(pad(data.encode('utf-8'),
+                                                      AES.block_size)))
 
     def decrypt(self, data):
         raw = b64decode(data)
---
+++ client_site.py
@@ -0,0 +1,37 @@
+# Coded by Yashraj Singh Chouhan
+import socket, threading
+import aes
+
+nickname = input("Choose your nickname: ")
+pwd = input("Input password to conversation: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket initialization
+client.connect(('127.0.0.1', 7976))  # connecting client to server
+
+
+def receive():
+    while True:  # making valid connection
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICKNAME':
+                client.send(nickname.encode('ascii'))
+            else:
+                aes.AESCipher(pwd).encrypt(message).decode('utf-8')
+                print(message)
+        except:  # case on wrong ip/port details
+            print("An error occured!")
+            client.close()
+            break
+
+
+def write():
+    while True:  # message layout
+        message = '{}: {}'.format(nickname, input(''))
+        aes.AESCipher(pwd).encrypt(message).decode('utf-8')
+        client.send(message.encode('ascii'))
+
+
+receive_thread = threading.Thread(target=receive)  # receiving multiple messages
+receive_thread.start()
+write_thread = threading.Thread(target=write)  # sending messages
+write_thread.start()
